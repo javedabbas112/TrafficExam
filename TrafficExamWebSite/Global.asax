d1a13d74b6f70142adc3ed99782d366471015248
soplist <sopcase@yahoo.com>
2017-05-03 16:43:48 +0800

    20170503

---
+++ TrafficExamWebSite/Global.asax
@@ -0,0 +1,44 @@
+﻿<%@ Application Language="C#" %>
+
+<script runat="server">
+
+    private static BLL.LoadController loadController = new BLL.LoadController();
+    private static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    
+    void Application_Start(object sender, EventArgs e) 
+    {
+        // Code that runs on application startup
+        //int i = 3;
+        //Application["class"] = 0;
+        Application["class"] = (List<Model.QuestionClass>)loadController.load("class");
+        logger.Info("class load");
+    }
+    
+    void Application_End(object sender, EventArgs e) 
+    {
+        //  Code that runs on application shutdown
+
+    }
+        
+    void Application_Error(object sender, EventArgs e) 
+    { 
+        // Code that runs when an unhandled error occurs
+
+    }
+
+    void Session_Start(object sender, EventArgs e) 
+    {
+        // Code that runs when a new session is started
+        
+    }
+
+    void Session_End(object sender, EventArgs e) 
+    {
+        // Code that runs when a session ends. 
+        // Note: The Session_End event is raised only when the sessionstate mode
+        // is set to InProc in the Web.config file. If session mode is set to StateServer 
+        // or SQLServer, the event is not raised.
+
+    }
+       
+</script>
---
+++ TrafficExamWebSite/QuestionClassManagement.aspx.cs
@@ -0,0 +1,62 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class QuestionClassManagement : System.Web.UI.Page
+{
+    private static QuestionClassBLL questionClassBLL;
+    private LoadController loadController;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        loadController = new LoadController();
+        questionClassBLL = new QuestionClassBLL();
+        initData(false);
+    }
+
+    private void initData(bool reload)
+    {
+        //List<Model.QuestionClass> claes = questionClassBLL.loadAllClasses();
+        //List<Model.QuestionClass> claes = (List<Model.QuestionClass>)loadController.load("class");
+        //Application["class"] = (List<Model.QuestionClass>)loadController.load("class");
+        if (reload)
+        {
+            updateApplicationClass();
+        }
+        this.classes.DataSource = (List<Model.QuestionClass>)Application["class"];
+        this.classes.DataBind();
+        //Application["class"] = 0;
+        //this.exLabel.Text = questionClassBLL.exceptionStr;
+    }
+
+    private void updateApplicationClass()
+    {
+        Application["class"] = (List<Model.QuestionClass>)loadController.load("class");
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        string className = this.classNameTextBox.Text;
+        questionClassBLL.addClass(className);
+        initData(true);
+    }
+
+    protected void linkbtnDelete_Click(object sender, EventArgs e)
+    {
+        string classID = (((LinkButton)sender).CommandArgument.ToString()).ToString();
+
+        if (classID != "")
+        {
+            if (!questionClassBLL.checkIsReference(classID))
+            {
+                questionClassBLL.deleteClass(classID);
+            }
+            
+        }
+        initData(true);
+    }
+}
